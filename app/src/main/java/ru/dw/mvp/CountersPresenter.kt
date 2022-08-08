23a29ac02fe60b1915a55330b0f12b3806bd2116
77den <77denisov@gmail.com>
2022-08-08 13:01:53 +0300

    Refactor code

--- app/src/main/java/ru/dw/mvp/CountersPresenter.kt
+++ app/src/main/java/ru/dw/mvp/CountersPresenter.kt
@@ -6,23 +6,8 @@ class CountersPresenter(
     private val model = CountersModel()
 
     fun onCounterClick(enum: ButtonCounter) {
-        when (enum) {
-            ButtonCounter.BtnOne -> {
-                onClick(enum)
-            }
-            ButtonCounter.BtnTwo-> {
-                onClick(enum)
-            }
-            ButtonCounter.BtnThree -> {
-                onClick(enum)
-            }
-        }
-    }
-
-    private fun onClick(enum: ButtonCounter) {
         val newValue = model.next(enum.position)
         view.setText(newValue.toString(), enum.position)
     }
-
 
 }
