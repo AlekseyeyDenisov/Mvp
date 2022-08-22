cf636e1241156ab1cbec515df3399890f58b0e56
77den <77denisov@gmail.com>
2022-08-22 10:03:29 +0300

    create retrofit

---
+++ app/src/main/java/ru/dw/mvp/core/network/UserDto.kt
@@ -0,0 +1,16 @@
+package ru.dw.mvp.core.network
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class UserDto (
+    @Expose
+    @SerializedName("id")
+    val id: Long,
+    @Expose
+    @SerializedName("login")
+    val login: String,
+    @Expose
+    @SerializedName("avatar_url")
+    val avatarUrl: String
+)
